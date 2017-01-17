8ed50e8e2659e388307fb6c73b6b5caedbbf06f5
乐意分享 <cheng-linw@hotmail.com>
2017-01-17 10:33:29 +0800

    autostart.sh
    auto start

---
+++ autostart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+declare -i t;
+t=0;
+XX=10; 
+while [ 1 ]
+do
+	if [ !`pgrep firefox` ]; then 
+		firefox &> /dev/null &
+		t=0;
+	fi
+
+	sleep 1;
+	t=$t+1;
+
+	if [[ $t -gt $XX ]]; then
+		echo "t > xx";
+		killall -9 firefox;
+		sleep 1;
+		firefox &> /dev/null &
+		t=0;
+	fi
+done
